78e22e5fb6217f21494b1cfd3b893f9dd45a595a
Chris <roos.chris.p@gmail.com>
2024-04-05 16:53:14 -0500

    building Navbar, working on what happens if userLoggedIn is false

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Feed from '@components/Feed'
 
-const home = () => {
+const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">AI-powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            Promptopia is an open-source AI prompting tool for the modern world to discover, create, and share creative prompts
+        </p>
+        
+        <Feed />
+    </section>
   )
 }
 
-export default home
+export default Home
