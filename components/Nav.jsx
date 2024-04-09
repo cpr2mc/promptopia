551efc9d88883138e65d39565c04e0f9e311824f
Chris <roos.chris.p@gmail.com>
2024-04-08 23:58:32 -0400

    added conditional button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = false;
+  const isUserLoggedIn = true;
 
   const [ providers, setProviders ] = useState(null);
 
@@ -61,14 +61,46 @@ const Nav = () => {
         ): (
           <>
             {providers && Object.values(providers).map((provider) => (
-              <button>
-                                
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className="black_btn"
+              >
+                  Sign In
               </button>
             ))}
           </>
         )}
       </div>
-
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => {}}
+            />
+          </div>
+        ): 
+        <>
+            {providers && Object.values(providers).map((provider) => (
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className="black_btn"
+              >
+                  Sign In
+              </button>
+            ))}
+          </>
+        }
+      </div>
     </nav>
   )
 }
