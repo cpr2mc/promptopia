601685553e7ec4c5a9061117f6296f83c65e9a7c
Chris <roos.chris.p@gmail.com>
2024-04-14 22:44:20 -0500

    fixed promptcard issue - missing component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,18 +7,34 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({ data, handleTagClick }) => {
   return(
     <div className="mt-16 prompt_layout">
-
+      {data.map((post) => (
+        <PromptCard 
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
     </div>
   )
 }
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
-
+  const [posts, setPosts] = useState([]);
+  
   const handleSearchChange = (e) => {
 
   }
   
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    }
+  }, []);
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -31,7 +47,7 @@ const Feed = () => {
           className="search_input peer"
         />
         <PromptCardList
-          data={[]}
+          data={posts}
           handleTagClick={() => {}}
         />
       </form>
